4b5bdb39cd5821fcc93a6cadb9e2e74bc4da38a0
Bucha1958 <okoriemmadu@gmail.com>
2023-10-30 14:05:15 +0100

    implemented a db class

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
+
+
+class DB:
+    """DB class 
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        create and save a new user
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        find user by an arbitrary keyword arguments
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        columns = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in columns:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if not user:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        update user with `user.id == user_id` using the provided
+        arbitrary keyword arguments
+        """
+        columns = User.__table__.columns.keys()
+        user = self.find_user_by(id=user_id)
+
+        for key in kwargs.keys():
+            if key not in columns:
+                raise ValueError
+
+        for k, v in kwargs.items():
+            setattr(user, k, v)
+        self._session.commit()
